551ad3062efebec04e35c28a8c1c83f911d4398b
SITO3 <bb@cc.com>
2021-09-28 17:53:41 +0800

    Try more

--- src/components/testPureWebRTC/Peer.js
+++ src/components/testPureWebRTC/Peer.js
@@ -1,27 +1,65 @@
-import WebRTC from './WebRTC';
+import io from 'socket.io-client';
 class Peer {
     constructor(peerName) {
-        let mslogger;
-        let webRTC=new WebRTC(peerName);
+        let msglogger;
+        let configuration = {
+            iceServers:
+                [{ urls: "stun:stun.stunprotocol.org" },
+                { urls: "stun:stun.l.google.com:19302" },
+                { urls: "turn:numb.viagenie.ca", credential: "turnserver", username: "sj0016092@gmail.com" }
+                ]
+        };
+        let dataChannel;
+        let ignoreOffer = false,makingOffer=false;
+        let peerConnection,polite=false;
+        let socket = io.connect(process.env.REACT_APP_SOCKET_HOST+":" + process.env.REACT_APP_SOCKET_PORT+"/testPureWebRTC", { transports: ['websocket'] });
+        socket.on("receiveRemoteDescription",async(remoteDescription)=>{
+            try{
+                const offerCollision = (remoteDescription.type == "offer") &&
+                                (makingOffer || peerConnection.signalingState != "stable");
+                ignoreOffer = !polite && offerCollision;
+                if (ignoreOffer) {
+                    return;
+                }
+                await peerConnection.setRemoteDescription(remoteDescription);
+                if (remoteDescription.type == "offer") {
+                    await peerConnection.setLocalDescription();
+                    socket.emit('sendLocalDescription', peerConnection.localDescription);
+                }
+            }catch(err) {
+                console.log(err);
+            }
+        });
         this.call=()=>{
-            //console.log("log="+(mslogger==null));
-            webRTC.call(true);            
-        }
-        this.hangUp=()=>{
-            webRTC.hangUp();
-            webRTC=null;
-            webRTC=new WebRTC(peerName);
-            webRTC.init();
-        }
-        this.init=()=>{
-            webRTC.init();
-        }
-        this.setDataChannelOpenHandler=(dataChannelOpenHandler)=>{
-            webRTC.setDataChannelOpenHandler(dataChannelOpenHandler);
+            //console.log(msglogger);
+            polite=true;
+            peerConnection=new RTCPeerConnection(configuration);
+            peerConnection.onicecandidate=(event)=>{iceCandidateEventHandler(event,msglogger);}
+            peerConnection.onnegotiationneeded=negotiationEventHandler; 
+            dataChannel= peerConnection.createDataChannel('chat');
         }
         this.setMsgLogger=(logger)=>{
-            mslogger=logger;
-            webRTC.setMsgLogger(logger);
+            msglogger=logger;
+        }
+//=======================================================================================================
+        function iceCandidateEventHandler(event,msglogger){
+            if (event.candidate==null){
+                msglogger.log("All ICE Candidates are sent");
+            } else {
+                console.log("Send an ICE Candidate");
+                socket.emit("sendICECandidate",event.candidate);
+            }
+        }
+        async function negotiationEventHandler(event){
+            try {
+                makingOffer = true;
+                await peerConnection.setLocalDescription();
+                socket.emit('sendLocalDescription', peerConnection.localDescription);
+              } catch(err) {
+                console.error(err);
+              } finally {
+                makingOffer = false;
+              }
         }
     }
 }
