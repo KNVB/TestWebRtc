e36182569de2c08253e1a2bbd708e994f9c35288
SITO3 <bb@cc.com>
2021-10-07 13:57:28 +0800

    The TestMeeting component working now.

--- server/testMeeting/TestMeeting.js
+++ server/testMeeting/TestMeeting.js
@@ -3,15 +3,21 @@ class TestMeeting{
         let peerList={};
         this.addPeer=(socket)=>{
             socket.on('disconnect', function () {
-                delete peerList[socket.id]
+                let peer=peerList[socket.id];
                 console.log("TestSimplePeer:Disconnected");
+                if (peer){
+                    console.log(peer.name+" leave the meeting.");
+                }
+                delete peerList[socket.id];
+                console.log("==================peer list===============");
                 console.log(peerList);
+                socket.broadcast.emit("removePeer",socket.id);
             });
             socket.on("hi",(peerName,calllBack)=>{
                 peerList[socket.id]={name:peerName,socketId:socket.id};
                 console.log("Received say hi from "+peerName+".");
                 socket.broadcast.emit("newPeer",peerList[socket.id]);
-                console.log("Broadcast newPeer to other peer.");
+                console.log("Broadcast newPeer ("+peerName+") to other peer.");
                 calllBack({"peerList":peerList})
             })
         }
--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import A from './components/testMeeting/A';
-//import C from './components/testMeeting/C';
-//import TestMeeting from './components/testMeeting/TestMeeting';
+import TestMeeting from './components/testMeeting/TestMeeting';
 import TestPureWebRTC from './components/testPureWebRTC/TestPureWebRTC';
 import TestSimplePeer from './components/testSimplePeer/TestSimplePeer';
 import Panel from "./components/share/panel/Panel";
@@ -11,9 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/' component={Panel}/>
-          <Route exact path='/a' component={A}/>
-  {/*     <Route exact path='/c' component={C}/>
-          <Route exact path='/testMeeting' component={TestMeeting}/>*/}
+          <Route exact path='/testMeeting' component={TestMeeting}/>
           <Route exact path='/testSimplePeer' component={TestSimplePeer}/>
           <Route exact path='/testPureWebRTC' component={TestPureWebRTC}/>
         </Switch>
