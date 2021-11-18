36f7d4e688175698d6b979bc9ed1f6ce2d1ab767
SITO3 <bb@cc.com>
2021-11-18 17:49:59 +0800

    Start to handle the connection switching problem.

--- src/components/testSocket/TestSocket.js
+++ src/components/testSocket/TestSocket.js
@@ -1,6 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import io from "socket.io-client";
 export default function TestSocket() {
+    let reducer = (state, action) => {
+        let result = { ...state };
+        switch (action.type) {
+            case "newPeer":
+                result.peerInfoList[action.newPeer.peerId] = {
+                    call: true,
+                    name: action.newPeer.name,
+                    socketId: action.newPeer.socketId,
+                };
+                break;
+            case "removePeer":
+                delete result.peerInfoList[action.peerId];
+                break;
+            case "setPeerList":
+                let temp = {};
+                Object.keys(action.peerInfoList).forEach((key) => {
+                    let peerInfo = action.peerInfoList[key];
+                    temp[key] = {
+                        call: false,
+                        name: peerInfo.name,
+                    };
+                });
+                result.peerInfoList = temp;
+                break;
+            default:
+                break;
+        }
+        return result;
+    };
     useEffect(() => {
         let peerName;
         let sUsrAg = navigator.userAgent;
@@ -19,27 +48,56 @@ export default function TestSocket() {
                 }
             }
         }
-        let socket = io(process.env.REACT_APP_SOCKET_URL + "a", {
+        let socket = io(process.env.REACT_APP_SOCKET_URL + "b", {
             transports: ["websocket"],
         });
         socket.on("connect", () => {
             const engine = socket.io.engine;
-            console.log(engine.transport.name);
+            let peerId=null;
+            let lastDisCntReason = "";
+            //console.log(engine.transport.name);
             console.log("Connect to server established.");
-            socket.io.on("reconnect", () => {
+            socket.emit("hi", peerName, response => {
+                peerId=response.peerId;
+                setItem({ type: "setPeerList", peerInfoList: response.peerList });
+            });
+            socket.on("disconnect", reason => {
+                console.log("socket disconnect event occur:" + reason);
+                lastDisCntReason = reason;
+            });
+            socket.on("newPeer", newPeer => {
+                setItem({ type: "newPeer", newPeer: newPeer });
+            });
+          
+            engine.on("reconnect", () => {
                 console.log("Reconnect successed.");
+                if (lastDisCntReason === "transport close") {
+                    lastDisCntReason = "";
+                    socket.emit("refreshSocketId",peerId);
+                }
             });
-            socket.on("disconnect", (reason) => {
-                console.log("socket disconnect event occur:"+reason);
+            socket.on("removePeer", peerId => {
+                setItem({ type: "removePeer", peerId: peerId });
             });
-            engine.on("close", (reason) => {
+
+            engine.on("close", reason => {
                 // called when the underlying connection is closed
-                console.log("Engine Close event occured:"+reason);
-            });    
+                console.log("Engine Close event occured:" + reason);
+                lastDisCntReason = reason;
+            });
         });
-    }, [])
+        return () => {socket.close()}
+    }, []);
+    let peerElementList = [];
+    const [items, setItem] = useReducer(reducer, { peerInfoList: {} });
+    if (items.peerInfoList) {
+        Object.keys(items.peerInfoList).forEach((peerId) => {
+            peerElementList.push(<div key={peerId}>{items.peerInfoList[peerId].name}</div>);
+        });
+    }
     return (
         <>
+            {peerElementList}
         </>
     )
 }
