612029b4d13c5d1fc385f4582c07582f53092a14
Roy <roy.cstsang@gmail.com>
2021-12-21 23:28:29 +0800

    2021-12-21 23:28

--- server/c/C.js
+++ server/c/C.js
@@ -1,25 +1,80 @@
 class C {
     constructor(io, path) {
+        let peerList = {};
+        let timeOut = 60; // in sec
+        let finalTimeOut = timeOut * 1000;        
+
         io.of(path).on("connection", socket => {
             console.log("Connection to Class C is established.");
-            /*
-            socket.on("askConnect",()=>{
-                console.log("ask connect event recevied.");
-                //socket.broadcast.emit("askConnect"); 
+            socket.on("hi", (peerName, calllBack) => {
+                let peerId = generateUID();
+                peerList[peerId] = { peerName: peerName, socketId: socket.id }
+                console.log("==================Receive Hi Event Start===============");
+                console.log("from:" + peerName);
+                socket.broadcast.emit("askConnect", { peerId: peerId, peerName: peerName });
+                calllBack({ peerId: peerId, "peerList": peerList });
+                console.log("==================peer list===============");
+                console.log(peerList);
+                console.log("==================Receive Hi Event End===============");
             });
-            */
-            socket.on("hi",peerName=>{
-                console.log("Receive Hi Event from "+peerName);
-                socket.broadcast.emit("askConnect",peerName);
+            socket.on("reconnectRequest", peer => {
+                console.log("==================Receive reconnectRequest Event Start==============="); 
+                console.log("Receive reconnect Event from " + peer.peerName);
+                console.log("peerid="+peer.peerId);
+                console.log("==================peer list===============");
+                console.log(peerList);
+                peerList[peer.peerId].socketId=socket.id;
+                peerList[peer.peerId].disconnectTime=null;
+                socket.broadcast.emit("askReconnect", peer.peerId);               
+                console.log("==================Receive reconnectRequest Event End==============="); 
             });
-            socket.on("reconnectRequest",peerName=>{
-                console.log("Receive reconnect Event from "+peerName);
-                socket.broadcast.emit("askReconnect",peerName);
-            });          
             socket.on('disconnect', (reason) => {
-                console.log("Disconnected:("+reason+")");
+                console.log("==================Receive Disconnect Event Start===============");
+                console.log("socket.id="+socket.id);
+                let removePeer=null,removePeerId = null;
+                for (const [peerId, peer] of Object.entries(peerList)) {
+                    if (peer.socketId === socket.id) {
+                        removePeer = peer;
+                        removePeerId=peerId;
+                        break;
+                    }
+                }
+                if (removePeer){
+                    console.log("peer:"+removePeer.peerName);
+                    if (reason === "client namespace disconnect") {               
+                        removePeerNow([removePeerId]);
+                    } else {
+                        peerList[removePeerId].disconnectTime=new Date();
+                    }
+                }
+                console.log("==================peer list===============");
+                console.log(peerList);
+                console.log("==================time out peer list===============");
+                console.log(timeOutPeer);           
+                console.log("==================Receive Disconnect Event End===============");
             });
         });
+
+        /*=======================================================*/
+        /*      Private Method                                   */
+        /*=======================================================*/
+        let generateUID = () => {
+            // I generate the UID from two parts here 
+            // to ensure the random number provide enough bits.
+            let firstPart = (Math.random() * 46656) | 0;
+            let secondPart = (Math.random() * 46656) | 0;
+            firstPart = ("000" + firstPart.toString(36)).slice(-3);
+            secondPart = ("000" + secondPart.toString(36)).slice(-3);
+            return firstPart + secondPart;
+        }
+        let removePeerNow=(removePeerIdList)=>{
+            /*
+            let peer=peerList[peerId];
+            console.log("Peer :" + peer.peerName + " Disconnected:(" + reason + ")");
+            delete peerList[peerId];
+            //io.of(path).broadcast.emit("removeId",peerId);
+            */
+        }
     }
 }
 module.exports = C;
--- src/components/c/Meeting.js
+++ src/components/c/Meeting.js
@@ -1,24 +1,36 @@
 import io from 'socket.io-client';
 export default class Meeting{
     constructor(peerName){
+        let peerId=null;
+        let peerList=null;
         let socket=null;
         console.log("Meeting Object constructor is called.")
         this.connect=()=>{
             socket= io(process.env.REACT_APP_SOCKET_URL + "c", {
                 transports: ["websocket"],
             });
-            socket.on("askConnect",peerName=>{
-                console.log("Receive Hi Event from "+peerName+".");
+            socket.on("askConnect",peer=>{
+                console.log("Receive Hi Event from "+JSON.stringify(peer)+".");
             });
             socket.on("askReconnect",peerName=>{
                 console.log("Receive askReconnect Event from "+peerName+".");
             });
             socket.io.on("reconnect", () => {
                 console.log("Reconnect successed.");
-                socket.emit("reconnectRequest",peerName);
+                socket.emit("reconnectRequest",{ peerId: peerId, peerName: peerName });
             });
-            socket.emit("hi",peerName);
-            
+            socket.on("removeId",peerId=>{
+                console.log("Receive remove id event ,peerId:"+peerId);
+            });
+            socket.emit("hi",peerName, response => {
+                peerId=response.peerId;
+                peerList=response.peerList;
+                console.log("==================Receive Hi Response Start===============");
+                console.log("peerId:"+peerId);
+                console.log("==================peer list===============");
+                console.log(peerList);
+                console.log("==================Receive Hi Response End===============");
+            });            
         }
         this.disconnect=()=>{
             if (socket){
