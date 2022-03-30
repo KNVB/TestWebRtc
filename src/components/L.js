c68302a62cd8dee623d067d6b2460c436c5d2d2d
Roy <roy.cstsang@gmail.com>
2022-03-30 17:13:08 +0800

    UI OK

--- src/components/L.js
+++ src/components/L.js
@@ -14,20 +14,20 @@ export default function L() {
         }
         setIndex(temp);
     }
-    let globalMessage=[];
+    let globalMessage = [];
     let guestList = [];
 
     globalMessage.push(<div key="msg_0">9999</div>);
-    for (let i=0;i<98;i++){
-        globalMessage.push(<div key={"msg_"+globalMessage.length}>dfsdfs</div>);
+    for (let i = 0; i < 98; i++) {
+        globalMessage.push(<div key={"msg_" + globalMessage.length}>dfsdfs</div>);
     }
     globalMessage.push(<div key="msg_99">1</div>);
     for (let i = 0; i < 100; i++) {
         guestList.push(
             <Row key={"guestList_" + i} className="m-0 p-0">
-                <Col className="border border-end-0 border-bottom-0 border-warning p-1" sm={4}><div style={{"height":"200px","width":"300px"}}>1</div></Col>
-                <Col className="border border-end-0 border-bottom-0 border-warning p-1" sm={4}><div style={{"height":"200px","width":"300px"}}>2</div></Col>
-                <Col className="border border-bottom-0 border-warning p-1" sm={4}><div style={{"height":"200px","width":"300px"}}>3</div></Col>
+                <Col className="border border-end-0 border-bottom-0 border-warning p-1" sm={4}><div style={{ "height": "200px", "width": "300px" }}>1</div></Col>
+                <Col className="border border-end-0 border-bottom-0 border-warning p-1" sm={4}><div style={{ "height": "200px", "width": "300px" }}>2</div></Col>
+                <Col className="border border-bottom-0 border-warning p-1" sm={4}><div style={{ "height": "200px", "width": "300px" }}>3</div></Col>
             </Row>
         );
     }
@@ -43,41 +43,44 @@ export default function L() {
                     (index === 0) &&
                     <div className="bg-danger  d-flex flex-grow-1">
                         1
-                    </div> 
+                    </div>
                 }
-                
+
                 {
                     (index === 1) &&
-                    <div className="d-flex flex-grow-1 m-0">
-                        <div className="bg-success d-flex flex-column flex-grow-1 p-1 rounded-3 text-white">
-                            <div>
-                                Guest List:
-                            </div>
-                            <div className="border border-white d-flex flex-grow-1 m-0 overflow-auto p-0 rounded-3" style={{ "height": "10em" }}>
-                                <Container className="flex-grow-1 m-0 p-1 rounded-3" fluid>
-                                    {guestList}
-                                </Container>
-                            </div>
-                        </div>
-                    </div>
+                    <Card className="bg-light d-flex flex-grow-1 m-0 p-0 rounded-5 text-white">
+                        <Card.Header className="m-0 p-0">
+                            Guest List:
+                        </Card.Header>
+                        <Card.Body className="bg-dark d-flex flex-column flex-grow-1 m-0 overflow-auto p-0" style={{ "height": "10em" }}>
+                            {guestList}
+                        </Card.Body>
+                    </Card>
                 }
-                
+
                 {
                     (index === 2) &&
-                    <div className="d-flex flex-grow-1">
-                        <div className="bg-primary d-flex flex-column flex-grow-1 text-white">
-                            <div>
-                                Global Message:
-                            </div>
-                            <div className="border border-dark d-flex flex-column flex-grow-1 m-0 p-1 rounded-3 overflow-auto" style={{ "height": "10em" }}>
-                                {globalMessage}
-                            </div>
-                        </div>
-                    </div>
-                }                
+                    <Container className="d-flex flex-grow-1 m-0 p-0" fluid>
+                        <Row className="d-flex flex-grow-1 m-0 p-0">
+                            <Col className="d-flex flex-grow-1 m-0 p-1 rounded-3 text-white" sm={6}>
+                                <Card className="bg-primary d-flex flex-grow-1 m-0 p-0">
+                                    <Card.Header className="m-0 p-0">
+                                        Global Message:
+                                    </Card.Header>
+                                    <Card.Body className="bg-info d-flex flex-column flex-grow-1 m-0 overflow-auto p-0" style={{ "height": "10em" }}>
+                                        {globalMessage}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col className="d-flex flex-column flex-grow-1 m-0 p-1 rounded-3" sm={6}>
+                                <textarea className="h-75 mb-2 rounded-3"></textarea>
+                                <Button >Send testing message to all peer</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                }
             </Card.Body>
             <Card.Footer className="border border-dark d-flex flex-row justify-content-around mt-1 rounded-3">
-                <Button >Send testing message to all peer</Button>
                 <div className="align-items-center bg-primary d-flex flex-row p-1 rounded-3 text-white">
                     <div className="m-1">Share Video:</div>
                     <DropdownButton title="Yes" variant="primary">
