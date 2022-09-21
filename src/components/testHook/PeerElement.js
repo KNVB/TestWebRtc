92ac1a9c65be8a27653b5be45a8eac0c316e6a78
Roy Tsang <cstsang@hko.gov.hk>
2022-09-21 12:11:46 +0800

    Enhance the PeerElement layout

--- src/components/testHook/PeerElement.js
+++ src/components/testHook/PeerElement.js
@@ -15,10 +15,10 @@ export default function PeerElement({ peer }) {
     }, [peer]);
     return (
         <div className="d-flex flex-row border border-info m-0 p-0 peer rounded-3 w-100">
-            <div className="border border-dark m-1 p-0 rounded-3 flex-grow-1 position-relative peer-videoCell">
+            <div className="border border-dark m-1 p-0 rounded-3 peer-cell">
                 <video autoPlay muted controls className='h-100 w-100 position-absolute rounded-3 video' ref={media} />
             </div>
-            <div className="border border-dark m-1 p-1 rounded-3 flex-grow-1 peer-nameCell text-break">
+            <div className="border border-dark m-1 p-1 rounded-3 peer-cell text-break">
                 Peer Name:<br />
                 {peer.peerName}<br />
                 Status:<br />
